ccfd970371576ac913a3f9541fdb1cee13099cf0
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2025-04-20 04:22:45 +0530

    added Fonts

--- src/@Types/UserDetails.tsx
+++ src/@Types/UserDetails.tsx
@@ -1,9 +1,8 @@
 export type UserDetails = {
-    username: string | null;
-    postText: string | null;
-    avatarUrl: string | null;
-    timer: string | null;
-    new_user: boolean;  
-    error : string | null;
-  };
-  
+  username: string | null;
+  postText: string | null;
+  avatarUrl: string | null;
+  timer: string | null;
+  new_user: boolean;  
+  error: string | null;
+};
